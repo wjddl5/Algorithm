3e975d8f31d3ab332fa9368c75b4bd8ed19cf245
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-02-19 10:24:17 +0900

    [Silver III] Title: N과 M (5), Time: 252 ms, Memory: 59456 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int N, M;
+    static int[] ar;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        ar = new int[N];
+        visit = new boolean[N];
+        for(int i=0; i<N; i++) {
+            ar[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ar);
+
+        find(0, "");
+        System.out.println(sb);
+
+        br.close();
+    }
+
+    public static void find(int depth, String str) {
+        if(depth == M) {
+            sb.append(str).append("\n");
+            return;
+        }
+        for(int i=0; i<N; i++) {
+            if(visit[i]) continue;
+            String str2 = (str + " " + ar[i]).trim();
+            visit[i] = true;
+            find(depth+1, str2);
+            visit[i] = false;
+        }
+
+            
+    }
+}
