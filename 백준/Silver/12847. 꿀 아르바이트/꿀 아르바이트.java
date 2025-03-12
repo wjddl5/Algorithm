6619f48241163d0628b1ac10cb66fb772e731eaf
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-03-12 13:13:12 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 208 ms, Memory: 27024 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n  = Integer.parseInt(st.nextToken());
+        int m  = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] ar = new int[n];
+        for(int i=0; i<n; i++) {
+            ar[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        long answer = 0;
+        long sum = 0;
+        for(int i=0; i<m; i++) {
+            answer += ar[i];
+        }
+
+        sum = answer;
+        int idx = 0;
+
+        for(int i=m; i<n; i++) {
+            sum = sum + ar[i] - ar[idx++];
+            if(answer < sum) answer = sum;
+        }
+
+        System.out.println(answer);
+    }
+
+}
