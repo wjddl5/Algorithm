ea74b00a9acf536579bc42e29878e1bd3ca71a27
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-03-23 16:38:00 +0900

    [Platinum V] Title: 곡선 자르기, Time: 1144 ms, Memory: 328544 KB, Score: 100 point -BaekjoonHub

--- 백준/Platinum/14865. 곡선 자르기/곡선 자르기.java
+++ 백준/Platinum/14865. 곡선 자르기/곡선 자르기.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
 
-    static int[][] tmp_aar, aar;
-    static Integer[] ar;
+    static ArrayList<Point> list = new ArrayList<>();
+    static int[][] ar;
     static int n, res1, res2, minIndex;
 
     public static void main(String[] args) throws Exception{
@@ -12,91 +12,82 @@ public class Main {
         StringTokenizer st = null;
 
         n = Integer.parseInt(br.readLine());
-        tmp_aar = new int[n][2];     
+        ar = new int[n][2];     
         int min = Integer.MAX_VALUE;
 
         for(int i=0; i<n; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            tmp_aar[i][0] = a; 
-            tmp_aar[i][1] = b;
+            ar[i][0] = a; 
+            ar[i][1] = b;
             int tmpMin = a + b;
             if(min > tmpMin) {
                 min = tmpMin;
                 minIndex = i;
             }
         }
-        setMin();
         find();
         check();
         System.out.println(res1+" "+res2);
     }
 
-    public static void setMin() {
-        aar = new int[n][2];
-        int zero = 0;
-        int mid = n-minIndex;
-        for(int i=minIndex; i<aar.length; i++) {
-            aar[zero][0] = tmp_aar[i][0]; 
-            aar[zero][1] = tmp_aar[i][1];
-            zero ++; 
-        }
-        for(int i=0; i<minIndex; i++) {
-            aar[mid][0] = tmp_aar[i][0];
-            aar[mid][1] = tmp_aar[i][1];
-            mid++;
-        }
-    }
-
     public static void find() {
-        ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i<aar.length-1; i++) {
-            if(aar[i][0] == aar[i+1][0]) {
-                if(aar[i][1] < 0 && aar[i+1][1] > 0) {
-                    list.add(aar[i][0]);
-                }
-                if(aar[i][1] > 0 && aar[i+1][1] < 0) {
-                    list.add(aar[i][0]);
+        ArrayList<Integer> tempList = new ArrayList<>();
+        for(int k=minIndex; k<minIndex+n-1; k++) {
+            int i = k;
+            int ii = k + 1;
+            if(i>=n) i -= n;
+            if(ii>=n) ii -= n;
+
+            if(ar[i][0] == ar[ii][0]) {
+                if(ar[i][1] < 0 && ar[ii][1] > 0 || ar[i][1] > 0 && ar[ii][1] < 0) {
+                    tempList.add(ar[i][0]);
                 }
             }
-        }
-        ar = new Integer[list.size()];
-        list.toArray(ar);
-
-        compare();
-    }
-
-    public static void compare() {
-        for(int i=0; i<ar.length; i+=2) {
-            int tmp1 = ar[i];
-            int tmp2 = ar[i+1];
+            if(tempList.size() == 2) {
+                int x1 = tempList.get(0);
+                int x2 = tempList.get(1);
+                if(x1 < x2) {
+                    Point p1 = new Point(x1, true);
+                    Point p2 = new Point(x2, false);
+                    list.add(p1);
+                    list.add(p2);
+                }else {
+                    Point p1 = new Point(x1, false);
+                    Point p2 = new Point(x2, true);
+                    list.add(p1);
+                    list.add(p2);
+                }
+                tempList.clear();
+            }
             
-            if(tmp1 > tmp2) {
-                ar[i] = tmp2;
-                ar[i+1] = tmp1;
-            }
         }
+        list.sort((o1, o2) -> o1.x - o2.x);
     }
 
     public static void check() {
-        for(int i=0; i<ar.length; i+=2) {
-            count(ar[i], ar[i+1]);
+        Stack<Integer> stack = new Stack<>();
+        int pointNum = 0;
+        for(Point p : list) {
+            if(p.isStart) stack.add(pointNum);
+            else {
+                int num = stack.pop();
+                if(stack.isEmpty()) res1 ++;
+                if(num == pointNum) res2 ++;
+
+                pointNum ++;
+            }
         }
     }
+}
 
-    public static void count(int x1, int x2) {
-        boolean chk1 = true;
-        boolean chk2 = true;
-        for(int i=0; i<ar.length; i+=2) {
-            if(x1 == ar[i] && x2 == ar[i+1]) continue;
+class Point  {
+    public int x;
+    public boolean isStart;
 
-            if(x1 > ar[i] && x2 < ar[i+1]) chk1 = false;
-
-            if(x1 < ar[i] && x2 > ar[i+1]) chk2 = false;
-        }
-        if(chk1) res1 ++;
-        if(chk2) res2 ++;
+    public Point(int x, boolean isStart)  {
+        this.x = x;
+        this.isStart = isStart;
     }
-
 }
