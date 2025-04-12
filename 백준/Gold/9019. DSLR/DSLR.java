167439c965632564334577baef35ef93e82d6e28
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-04-12 23:00:23 +0900

    [Gold IV] Title: DSLR, Time: 2144 ms, Memory: 295116 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.java
+++ 백준/Gold/9019. DSLR/DSLR.java
@@ -3,57 +3,76 @@ import java.util.*;
 
 public class Main {
 
-    static int A, B;
-    static String[] ar, dslr = {"D", "S", "L", "R"};
-    static StringBuilder sb = new StringBuilder();
+    static int target;
+    static int[] from;
+    static char[] how;
+    static boolean[] visit;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
+        
         for(int tc=0; tc<T; tc++) {
-            ar = new String[10000];
+            visit = new boolean[10000];
+            how = new char[10000];
+            from = new int[10000];
+
             st = new StringTokenizer(br.readLine());
-            A = Integer.parseInt(st.nextToken());
-            B = Integer.parseInt(st.nextToken());
-            cal(A);
-            sb.append(ar[B]).append("\n");
+            int n = Integer.parseInt(st.nextToken());
+            target = Integer.parseInt(st.nextToken());
+
+            bfs(n);
+            sb.append(print()).append('\n');
         }
         System.out.println(sb);
     }
 
-    public static void cal(int n) {
+    public static void bfs(int n) {
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(n);
-        ar[n] = "";
+        visit[n] = true;
+        from[n] = -1;
+
         while(!queue.isEmpty()) {
             int num = queue.poll();
-            for(int i=0; i<4; i++) {
-                int tmpnum = dslr(num, dslr[i]);
-                if(ar[tmpnum] == null) {
-                    queue.offer(tmpnum);
-                    ar[tmpnum] = ar[num] + dslr[i];
-                }
-                if(tmpnum == B) return;
+
+            for(char control : new char[] {'D', 'S', 'L', 'R'}) {
+                int next = dslr(num, control);
+                if(visit[next]) continue; 
+                
+                queue.offer(next);
+                visit[next]  = true;
+                from[next] = num;
+                how[next]  = control;
+                if(next == target) return;
             } 
         }
     }
 
-    public static int dslr(int n, String str) {
-        switch (str) {
-            case "D":
-                n = (n*2)%10000;
-                break;
-            case "S":
-                if(n == 0) n = 9999;
-                else n = n-1;
-                break;
-            case "L":
-                n = (n%1000*10) + (n/1000);
-                 break;
-            case "R":
-                n = (n%10*1000) + (n/10);
+    public static String print() {
+        StringBuilder sbr = new StringBuilder();
+        int num = target;
+        while(from[num] != -1)  {
+            sbr.append(how[num]);
+            num = from[num];
         }
-        return n;
+        return sbr.reverse().toString();
+    }
+
+    public static int dslr(int n, char str) {
+        switch (str) {
+            case 'D':
+                return (n*2)%10000;
+            case 'S':
+                return n == 0 ? 9999 : n -1;
+            case 'L':
+                return (n%1000*10) + (n/1000);
+            case 'R':
+                return (n%10*1000) + (n/10);
+        }
+        return -1;
     }
 
 }
