c78218d4a96fb038c7d2958497adab5add2ca66d
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-04-28 21:03:43 +0900

    [Gold III] Title: 세 용액, Time: 152 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static long[] ar, answer=new long[3];
+    static long min=Long.MAX_VALUE;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        ar = new long[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            ar[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(ar);
+        find();
+        System.out.println(answer[0]+" "+answer[1]+" "+answer[2]);
+    }
+    
+    static void find() {
+        for(int left=0; left<N-2; left++) {
+            int mid = left + 1;
+            int right = N - 1;
+
+            while(mid < right) {
+                long sum = ar[left] + ar[mid] + ar[right];       
+                minCheck(left, mid, right, sum);
+                
+                if(sum == 0) return;
+                
+                if(sum < 0) {
+                    mid++;
+                    continue;
+                } else {
+                    right--;
+                    continue;
+                }
+            }
+        }
+    }
+
+    static void minCheck(int l, int m, int r, long tmpsum) {
+        long sum = Math.abs(tmpsum);    
+        if(min > sum) {
+            min = sum;
+            answer[0] = ar[l];
+            answer[1] = ar[m];
+            answer[2] = ar[r];
+        }  
+    }
+
+}
