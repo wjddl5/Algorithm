5f2194a71e48883015d5f06b673c5802ae2e5aec
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-05-19 15:48:23 +0900

    [Gold III] Title: 텀 프로젝트, Time: 916 ms, Memory: 297192 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
     static int N, count;
-    static Node[] ar;
-    static boolean[] visit;
-    static int[] team;
+    static int[] ar;
+    static boolean[] visit, team;
     
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,13 +15,13 @@ public class Main {
         while(T-- > 0) {
             count = 0;
             N = Integer.parseInt(br.readLine());
-            ar = new Node[N+1];
+            ar = new int[N+1];
             visit = new boolean[N+1];
-            team = new int[N+1];
+            team = new boolean[N+1];
 
             st = new StringTokenizer(br.readLine());
             for(int i=1; i<=N; i++) {
-                ar[i] = new Node(i, Integer.parseInt(st.nextToken()));
+                ar[i] = Integer.parseInt(st.nextToken());
             }
 
             find();
@@ -33,32 +32,24 @@ public class Main {
 
     static void find() {
         for(int i=1; i<=N; i++) {
-            if(visit[i]) continue;
-            dfs(ar[i]);
+            if(!visit[i]) dfs(i);
         }
     }
     
-    static void dfs(Node node) {
-        if(team[node.num] == 0) {
-            team[node.num] = 1;
-            dfs(ar[node.next]);
+    static void dfs(int num) {
+        visit[num] = true;
+        int next = ar[num];
+
+        if(!visit[next]) {
+            dfs(next);
         }
-        else if(team[node.num] == 1 && !visit[node.num]) {
-            for(Node tmpNode=node; team[tmpNode.num]!=2; tmpNode=ar[tmpNode.next]) {     
-                team[tmpNode.num] = 2;
+        else if(!team[next]) {
+            for(int i=next; i!=num; i=ar[i]) {     
                 count++;
             }
+            count++;
         }    
-        visit[node.num] = true;
+        team[num] = true;
     }
 
-}
-
-class Node {
-    int num;
-    int next;
-    Node(int num, int next) {
-        this.num = num;
-        this.next = next;
-    }
 }
