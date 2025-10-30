a4b3449f4231a1d059cb2cc01554bafaed9819d2
JeongYi <165870505+wjddl5@users.noreply.github.com>
2025-10-30 17:07:08 +0900

    [Gold III] Title: 경사로, Time: 84 ms, Memory: 12772 KB -BaekjoonHub

--- 백준/Gold/14890. 경사로/경사로.java
+++ 백준/Gold/14890. 경사로/경사로.java
@@ -2,17 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, L, total;
+
+    static int N, L, answer;
     static int[][] map;
+    static int[] dx = {1, 0}, dy = {0, 1};
+
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = null;
+
+
+        st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         L = Integer.parseInt(st.nextToken());
 
         map = new int[N][N];
+
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
@@ -21,80 +27,66 @@ public class Main {
         }
 
         find();
-        System.out.println(total);
+
+        System.out.println(answer);
     }
 
     static void find() {
         for(int i=0; i<N; i++) {
-            findRow(i);
-            findCol(i);
+            move(0, i, 0);
+            move(i, 0, 1);
         }
     }
 
-    static void findRow(int r) {
-        int now =  map[r][0];
-        int cnt = 1;
-        boolean chk = false;
+    static void move(int x, int y, int d) {
+        int now = map[x][y];
+        boolean isDown = false;
+        int floor = 1;
 
-        for(int c=1; c<N; c++) {
-            if(map[r][c] == now) {
-                cnt++;
-                if(chk && cnt>=L) {
-                    chk = false;
-                    if(L == 1) cnt = 1;
-                    else cnt = 0;
-                }
-                continue;
-            } else if(map[r][c] == now+1) {
-                if(chk || cnt < L) return;
-                now++;
-                cnt = 1;
-                continue;
-            } else if(map[r][c] == now-1){
-                if(chk && cnt < L) return;
-                chk = true;
-                now--;
-                cnt = 1;
-                continue;
-            } else {
-                return;
+        while(true) {
+            x += dx[d];
+            y += dy[d];
+
+            if(x >= N || y >= N) {
+                if(isDown && floor < L) break;
+                answer++;
+                break;
             }
-        }
-        if(chk && cnt < L) return;
-        total++;
-    }
 
-    static void findCol(int c) {
-        int now =  map[0][c];
-        int cnt = 1;
-        boolean chk = false;
+            int next = map[x][y];
+            int gap = now - next;
+            now = next;
 
-        for(int r=1; r<N; r++) {
-            if(map[r][c] == now) {
-                cnt++;
-                if(chk && cnt>=L) {
-                    chk = false;
-                    if(L == 1) cnt = 1;
-                    else cnt = 0;
-                }
-                continue;
-            } else if(map[r][c] == now+1) {
-                if(chk || cnt < L) return;
-                now++;
-                cnt = 1;
-                continue;
-            } else if(map[r][c] == now-1){
-                if(chk && cnt < L) return;
-                chk = true;
-                now--;
-                cnt = 1;
-                continue;
-            } else {
-                return;
+            // 내려갈 공간 충분함
+            if(isDown && floor >= L) {
+                isDown = false;
+                floor = 0;
             }
+
+            // 2칸 이상 차이남
+            if(Math.abs(gap) >= 2) break;
+
+            // 내려가는 중에 높이 다른 칸 나옴
+            if(isDown && gap > 0) break; 
+
+            // 내려감
+            if(gap == 1) {
+                isDown = true;
+                floor = 1;
+                continue;
+            }
+
+            // 올라감
+            if(gap == -1) {
+                // 올라갈 공간 부족
+                if(floor < L) break;
+                floor = 1;
+                continue;
+            }
+
+            // 진행
+            floor++;
         }
-        if(chk && cnt < L) return;
-        total++;
     }
 
 }
